d12684ed8e4a5d0166acb371dc58f405507b58e9
vito <vito@staritgp.com>
2020-10-29 12:59:11 +0800

    project init

---
+++ backend-example/src/main/java/com/vito/backendexample/BackendExampleApplication.java
@@ -0,0 +1,13 @@
+package com.vito.backendexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendExampleApplication.class, args);
+	}
+
+}
---
+++ backend-example/src/test/java/com/vito/backendexample/BackendExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vito.backendexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
