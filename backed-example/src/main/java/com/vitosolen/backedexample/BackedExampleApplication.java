0a09135f0e72511844ecd2d59bcdfe88db54a9ec
vito <vito@staritgp.com>
2020-10-29 11:47:17 +0800

    项目初始化

---
+++ backed-example/src/main/java/com/vitosolen/backedexample/BackedExampleApplication.java
@@ -0,0 +1,13 @@
+package com.vitosolen.backedexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackedExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackedExampleApplication.class, args);
+	}
+
+}
---
+++ backed-example/src/test/java/com/vitosolen/backedexample/BackedExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitosolen.backedexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackedExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
